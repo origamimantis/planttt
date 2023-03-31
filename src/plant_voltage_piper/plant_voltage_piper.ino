f11063cf330c6c026159e49362b7f222ef5b8258
eric zhang <ezhang323@gmail.com>
2023-03-31 15:31:06 -0700

    fix huge reading fluctuations

--- src/plant_voltage_piper/plant_voltage_piper.ino
+++ src/plant_voltage_piper/plant_voltage_piper.ino
@@ -1,19 +1,38 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN,LOW);
+  
+  // use the pullup resistor to avoid huge noise
+  pinMode(A0,INPUT_PULLUP);
 }
 
-int g = 0;
 int x = 0;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  // wait for python reader to send a start signal, no need to read it
+float sum = 0;
+int lastupdate = 0;
+int n = 0;
 
-  g = analogRead(A0)*5/1024;
-  x = analogRead(A1)*5/1024;
-  Serial.println(String(x-g) + " " + String(millis()));
-  delay(50);
+void loop() {
+
+  // read
+  x = analogRead(A0);
+
+  // add to the current cycle's average and track number of samples
+  sum += x;
+  n += 1;
+
+  // check if enough time has passed since the previous serial update
+  int now = millis();
+  if (now - lastupdate > 50)
+  {
+    // calculate the average input reading
+    float avg = sum/n;
+    Serial.println(String(avg) + " " + String(now));
+
+    // reset cycle-tracking vars
+    lastupdate=now;
+    sum = 0;
+    n = 0;
+  }
+
 }
